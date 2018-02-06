306bfefc85ebebae0f5cd3ac8fa38cded924ad16
Aaron Pietsch <apietsch4@gmail.com>
2018-02-05 19:58:54 -0800

    load test phase 1

---
+++ events/test/artillery/my-functions.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+    generateData
+};
+
+// Make sure to "npm install faker" first.
+// const Faker = require('faker');
+
+function generateData(userContext, events, done) {
+    // generate data with Faker:
+    var listingId = String(Math.floor(Math.random() * 1000000));
+    var hostId = String(Math.floor(Math.random() * 100000));;
+    // add variables to virtual user's context:
+    userContext.vars.listingId = listingId;
+    userContext.vars.hostId = hostId;
+    // continue with executing the scenario:
+    return done();
+}
