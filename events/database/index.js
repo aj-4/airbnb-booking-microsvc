31a417e6af31cdaaad0d3ebaac173a5c6b833461
Aaron Pietsch <apietsch4@gmail.com>
2018-02-06 17:04:36 -0800

    revert to prior schema

--- events/database/index.js
+++ events/database/index.js
@@ -1,11 +1,13 @@
 const init = require('./indexCassandra');
 const getBook = require('./bookRetrieval');
+const getView = require('./viewRetrieval');
 const addBook = require('./bookInsertion');
 const addView = require('./viewInsertion');
 
 module.exports = {
     init,
     getBook,
+    getView,
     addBook,
     addView
 }
